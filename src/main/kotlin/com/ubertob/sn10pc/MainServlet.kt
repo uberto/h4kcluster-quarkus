c6fb2bb002656ed8b1a9cdf6e50d10d6352d2ee2
uberto barbini <uberto.barbino@springernature.com>
2020-07-10 16:42:21 +0100

    added http4k

---
+++ src/main/kotlin/com/ubertob/sn10pc/MainServlet.kt
@@ -0,0 +1,23 @@
+package com.ubertob.sn10pc
+
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.servlet.HttpHandlerServlet
+import javax.servlet.Servlet
+import javax.servlet.annotation.WebServlet
+
+
+@WebServlet("/")
+open class RestServlet: Servlet by HttpHandlerServlet(
+        MyHandler())
+
+
+
+class MyHandler : (Request) -> Response {
+    override fun invoke(request: Request): Response {
+        return Response(OK).body("<html><body><h1>Hello!!!!!!!</h1></body></html>")
+    }
+
+}
+
